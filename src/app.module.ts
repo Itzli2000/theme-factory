69f9dcb13c26b1500f10547fbec1821ce2882eaa
Itzli Molina <itzli2000@msn.com>
2025-07-13 10:20:01 -0600

    chore(env): update .env.template and app.module.ts for configuration
    - Added NODE_ENV, JWT_SECRET, and JWT_EXPIRES_IN to .env.template for environment variable management.
    - Integrated ConfigModule and TypeOrmModule in app.module.ts for dynamic database configuration based on environment variables.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,26 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: parseInt(configService.get('DB_PORT') || '5432'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('POSTGRES_PASSWORD'),
+        database: configService.get('POSTGRES_DB'),
+        autoLoadEntities: true,
+        synchronize: configService.get('NODE_ENV') !== 'production',
+        logging: configService.get('NODE_ENV') === 'development',
+      }),
+    }),
+  ],
   controllers: [],
   providers: [],
 })
