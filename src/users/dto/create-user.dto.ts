1ca717db1aabafefa0dff9bf4e28ed77e8168fd4
Itzli Molina <itzli2000@msn.com>
2025-07-13 11:01:34 -0600

    feat(Users): enhance user DTOs and add response structure
    - Introduced CreateUserDto with validation for email, password, firstName, lastName, and isActive fields.
    - Added LoginUserDto for user authentication with email and password validation.
    - Created UserResponseDto to structure user response data, including fields for ID, email, firstName, lastName, isActive, createdAt, updatedAt, and password (excluded from response).
    - Updated UpdateUserDto to omit the password field from the CreateUserDto.
    
    This commit improves user data handling and validation across the application.

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,24 @@
-export class CreateUserDto {}
+import { IsBoolean, IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+
+  @IsString()
+  @IsNotEmpty()
+  firstName: string;
+
+  @IsString()
+  @IsNotEmpty()
+  lastName: string;
+
+  @IsBoolean()
+  @IsNotEmpty()
+  isActive: boolean;
+}
