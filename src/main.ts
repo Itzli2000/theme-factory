813d33c2d54bad580d16cc90aa01864aab36ff95
Itzli Molina <itzli2000@msn.com>
2025-07-15 07:42:48 -0600

    feat(CORS): enable CORS for API with specified methods and headers
    - Configured CORS in the main application file to allow requests from any origin.
    - Specified allowed methods (GET, POST, PUT, DELETE, OPTIONS) and headers (Content-Type, Authorization) for enhanced API interaction.
    
    This commit improves the API's accessibility by allowing cross-origin requests, facilitating integration with various clients.

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,12 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
   app.setGlobalPrefix('api');
 
   app.useGlobalPipes(
